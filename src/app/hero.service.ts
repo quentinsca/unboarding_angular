6233bda791edc8e7a0f0c90caeeb14f63eec7b80
quentinsca <quentinsc@theodo.fr>
2022-02-16 17:55:59 +0100

    Correction nom variable Hero-service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,22 +3,30 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  private heroesUrl = 'api/heroes';
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    const hero = HEROES.find((hero) => hero.id === id)!;
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
 
+  private log(message: string): void {
+    this.messageService.add(message);
+  }
+
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add(`Heroes Component: heroes fetched`);
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 }
